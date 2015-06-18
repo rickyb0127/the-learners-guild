90ff78e64162bf1d71455f7af4a27026b7cf9092
eric bartsch <bartsched@gmail.com>
2015-06-17 20:38:32 -0600

    user can only see edit links on their own show page

--- app/assets/javascripts/profile-show.js
+++ app/assets/javascripts/profile-show.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
+  $(".edit-picture").hide();
+
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
+  });
+
+  $(".edit-picture-link").click(function () {
+    $(".edit-picture").show();
+  });
+
 });
